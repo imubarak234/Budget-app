56f750ee64e55e9e0beb462493b2e61045dfa923
imubarak234 <imubarak2424@gmail.com>
2022-04-26 08:12:27 +0100

    devise setup

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,4 +2,6 @@ class Group < ApplicationRecord
   belongs_to :author, class_name: 'User'
 
   has_many :logs, foreign_key: 'group_id'
+
+  validates :name, presence: true, length: {minimum:3, maximum:100}
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,4 +12,10 @@ Rails.application.routes.draw do
 
   resources :logs
 
+  Rails.application.routes.draw do
+    devise_for :users, controllers: {
+      sessions: 'users/sessions'
+    }
+  end
+
 end
