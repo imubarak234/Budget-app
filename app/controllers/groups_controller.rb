0a6298ae45df0c537b204f7f08dea662e5f7f65b
imubarak234 <imubarak2424@gmail.com>
2022-04-28 23:48:02 +0100

    catagory page

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,4 +1,13 @@
 class GroupsController < ApplicationController
   before_action :authenticate_user!
-  def index; end
+  def index
+    @groups = Group.all
+  end
+
+  def new
+    @group_new = current_user.group.new
+    respond_to do | format |
+      format.html { render :new, locals: { groups: @group_new } }
+    end
+  end
 end
