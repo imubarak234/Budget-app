bf5107194cd6e19a2a0b51d9241bc31e9ba6396d
imubarak234 <imubarak2424@gmail.com>
2022-04-29 17:07:45 +0100

    index transactions

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -1,3 +1,15 @@
 class RecordsController < ApplicationController
   before_action :authenticate_user!
+
+  def index
+    @records = []
+    @group = Group.find(params[:group_id])
+    @var = 0
+    record_logs = Log.where(group_id: 3).pluck(:record_id)
+    record_logs.each do | n |
+      record = Record.find(n)
+      @records.push(record)
+      @var += record.amount
+    end
+  end
 end
