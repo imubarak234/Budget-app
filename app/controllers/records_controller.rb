6accbdd53b483086efa14d5335a8f868761b5c3e
imubarak234 <imubarak2424@gmail.com>
2022-04-30 00:21:11 +0100

    new transactions

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -5,7 +5,7 @@ class RecordsController < ApplicationController
     @records = []
     @group = Group.find(params[:group_id])
     @var = 0
-    record_logs = Log.where(group_id: 3).pluck(:record_id)
+    record_logs = Log.where(group_id: params[:group_id]).pluck(:record_id)
     record_logs.each do | n |
       record = Record.find(n)
       @records.push(record)
