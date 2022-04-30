d04f490e4940416410590bb6b2bcd33af354ec2f
imubarak234 <imubarak2424@gmail.com>
2022-04-30 09:05:26 +0100

    group_new_spec

--- spec/integration/group_new_spec.rb
+++ spec/integration/group_new_spec.rb
@@ -1,4 +1,4 @@
-RSpec.describe 'Group Index', type: :feature do
+RSpec.describe 'Group New', type: :feature do 
   before :each do
     one = User.create(email: 'user1@gmail.com', password: 'password', name: 'User1')
 
@@ -10,13 +10,16 @@ RSpec.describe 'Group Index', type: :feature do
     click_button 'Log in'
   end
 
-  it 'Check for exisiting content' do
-    visit '/'
+  it 'Check for appropiate responds from form' do
+    visit '/groups/new'
+
+    fill_in 'Name', with: 'testings'
+    choose('groups_icon_bi_bi-award-fill')
+    click_button 'Save'
 
     group_one = Group.create(name: "first group", icon: "bi bi-search", author_id: 1)
     group_two = Group.create(name: "first group", icon: "bi bi-search", author_id: 1)
 
-    expect(page).to have_content(group_one.name)
-    expect(page).to have_content(group_two.name)
+    expect(page).to have_content("Catagory created successfully")
   end
 end
