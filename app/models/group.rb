9b4b6071ea4825239aca5250e2179c8117e242aa
imubarak234 <imubarak2424@gmail.com>
2022-04-30 14:31:22 +0100

    corrections

--- app/models/group.rb
+++ app/models/group.rb
@@ -4,4 +4,14 @@ class Group < ApplicationRecord
   has_many :logs, foreign_key: 'group_id'
 
   validates :name, presence: true, length: { minimum: 3, maximum: 100 }
+
+  def self.totals(data)
+    logs = Log.where(group_id: data).order(created_at: :desc).pluck(:record_id)
+    var = 0
+    logs.each do |n|
+      record = Record.find(n)
+      var += record.amount
+    end
+    var = var.round(2)
+  end
 end
