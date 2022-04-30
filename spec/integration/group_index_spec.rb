24c4577a523625b6c87e2cd73b0f70f57edf94f8
imubarak234 <imubarak2424@gmail.com>
2022-04-30 08:44:24 +0100

    group-index-spec

--- spec/integration/group_index_spec.rb
+++ spec/integration/group_index_spec.rb
@@ -1,11 +1,21 @@
 RSpec.describe 'Group Index', type: :feature do
   let(:user) { FactoryBot.create(:user) } 
+  before :each do
+    one = User.create(email: 'user1@gmail.com', password: 'password', name: 'User1')
+
+    Group.create(name: "first group", icon: "bi bi-search", author_id: 1)
+
+    visit '/users/sign_in'
+    fill_in 'Email', with: 'user1@gmail.com'
+    fill_in 'Password', with: 'password'
+    click_button 'Log in'
+  end
 
   it 'Check for exisiting content' do
-    visit '/groups'
+    visit '/'
 
-    group_one = Group.create(name: "first group", icon: "icon1", author_id: 1)
-    group_two = Group.create(name: "first group", icon: "icon1", author_id: 1)
+    group_one = Group.create(name: "first group", icon: "bi bi-search", author_id: 1)
+    group_two = Group.create(name: "first group", icon: "bi bi-search", author_id: 1)
 
     expect(page).to have_content(group_one.name)
     expect(page).to have_content(group_two.name)
