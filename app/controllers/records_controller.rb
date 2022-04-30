6a4c2db7cb8ed9fb6aac5287b6d55c8b65b8e89e
imubarak234 <imubarak2424@gmail.com>
2022-04-30 06:45:12 +0100

    linters

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -6,7 +6,7 @@ class RecordsController < ApplicationController
     @group = Group.find(params[:group_id])
     @var = 0
     record_logs = Log.where(group_id: params[:group_id]).order(created_at: :desc).pluck(:record_id)
-    record_logs.each do | n |
+    record_logs.each do |n|
       record = Record.find(n)
       @records.push(record)
       @var += record.amount
@@ -14,7 +14,7 @@ class RecordsController < ApplicationController
     @var = @var.round(2)
   end
 
-  def new 
+  def new
     @record_new = current_user.records.new
     respond_to do |format|
       format.html { render :new, locals: { records: @record_new } }
@@ -25,14 +25,12 @@ class RecordsController < ApplicationController
     grouping = params.require(:records)[:group]
     record_create = Record.new(params.require(:records).permit(:name, :amount))
     record_create.update(author_id: current_user.id)
-    
+
     respond_to do |format|
       format.html do
         if record_create.save
           grouping.each do |n|
-            if n.to_i > 0
-              Log.create(group_id: n.to_i, record_id: record_create.id)
-            end
+            Log.create(group_id: n.to_i, record_id: record_create.id) if n.to_i.positive?
           end
           flash[:success] = 'Transaction created successfully'
           redirect_to group_records_path
@@ -43,5 +41,4 @@ class RecordsController < ApplicationController
       end
     end
   end
-  
 end
